c791f2d15d7a2b438ef7759cc14b955bf98828d9
unknown <lujinming@cn.net.ntes>
2018-07-25 16:00:28 +0800

    优化

--- formdataServer.js
+++ formdataServer.js
@@ -1,14 +1,11 @@
 var http = require('http');
 var fs = require('fs');
-var util = require('util');
-var querystring = require('querystring');
-
 
 const PORT = 8888;
 
-const formdataServer = http.createServer();
+const formDataServer = http.createServer();
 
-formdataServer.on('request', (req, res) =>{
+formDataServer.on('request', (req, res) =>{
 
     if(req.method.toLowerCase() === 'post'){
         if(req.headers['content-type'].indexOf('multipart/form-data') !== -1){
@@ -26,40 +23,39 @@ function parseFile(req, res){
     res.setHeader('Content-Type', 'text/html;charset=UTF-8');
     req.setEncoding('binary');  // 一定要设置response的编码为binary否则会图片打不开
 
-    var body = '';
-    var fileName = '';
+    let body = '';
+    let fileName = '';
 
-    var boundary = req.headers['content-type'].split(';')[1].replace(' boundary=','');
+    let boundary = req.headers['content-type'].split(';')[1].replace(' boundary=','');
 
     req.on('data', (chunk) => {
         body += chunk;
     });
     req.on('end', () =>{
-        var file = querystring.parse(body, '\r\n', ":");
-        if(file['Content-Type'].indexOf('image') !== -1){
-            var fileInfo = file['Content-Disposition'].split(';');
-            for(var index in fileInfo){
-                if(fileInfo[index].indexOf('filename=') !== -1){
-                    fileName = fileInfo[index].substring(11, fileInfo[index].length-1);
+        let fileNameReg = /filename="([^"]*)"/;
+        let fileTypeReg = /(image\/([^\s]*)\s)/;
+        let fileType = null;
+        let contentType = null;
+        if(fileNameReg.test(body)){
+            fileName = RegExp.$1;
+        }
+        if(fileTypeReg.test(body)){
+            contentType = RegExp.$1;
+            fileType = RegExp.$2;
+        }
+
+        if(fileType){
+
+            let upperBoundary = body.indexOf(contentType) + contentType.length;
+            let shorterData = body.slice(upperBoundary, -1 * ('--'+boundary+'--').length);
+            let imageData = shorterData.replace(/^\s\s*/, '');
+
+            fs.writeFile(fileName, imageData, 'binary', (err) => {
+                if(err){
+                    res.end('图片上传失败');
+                } else{
+                    res.end('图片上传完成');
                 }
-            }
-            var entireData = body.toString();
-            var contentTypeRegx = /Content-Type: image\/.* /;
-            var contentType = file['Content-Type'].substring(1);
-
-            var upperBoundary = entireData.indexOf(contentType) + contentType.length;
-
-            var shorterData = entireData.substring(upperBoundary);
-
-
-            var binaryDataAlmost = shorterData.replace(/^\s\s*/, '').replace(/\s\s*$/,'');
-
-            var binaryData = binaryDataAlmost.substring(0, binaryDataAlmost.indexOf('--'+boundary+'--'));
-
-
-            fs.writeFile(fileName, binaryData, 'binary', (err) => {
-                console.log(err);
-                res.end('图片上传完成');
             })
         }else{
             res.end('只能上传图片');
@@ -83,7 +79,7 @@ function sendHTML(req, res){
 
 
 
-formdataServer.listen(PORT, () => {
+formDataServer.listen(PORT, () => {
     console.log("Server running at http://localhost:" + PORT);
 });
 
